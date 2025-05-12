2b1753cab80173cc038de68177ad3ea96c009995
quinirix <pchidung57@outlook.com.vn>
2025-05-12 14:11:17 +0700

    Update loader-kinovrise_Version2.ps1

--- loader-kinovrise_Version2.ps1
+++ loader-kinovrise_Version2.ps1
@@ -1,4 +1,9 @@
-# Loader chính, chạy qua irm | iex, sẽ popup CMD menu
+# Show greeting in PowerShell
+$user = [System.Environment]::UserName
+$hostName = $env:COMPUTERNAME
+Write-Host "`n========= Welcome, $user! Computer: $hostName =========`n"
+
+# Create and start batch file for menu in a new CMD window
 $tmpBat = [System.IO.Path]::GetTempFileName() -replace '\.tmp$', '.bat'
 $batContent = @'
 @echo off
