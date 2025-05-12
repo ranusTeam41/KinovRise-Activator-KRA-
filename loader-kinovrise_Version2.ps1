44442e8e22466afc306d8c06568e46b4895ca662
quinirix <pchidung57@outlook.com.vn>
2025-05-12 13:57:18 +0700

    Add files via upload

---
+++ loader-kinovrise_Version2.ps1
@@ -0,0 +1,9 @@
+# Loader chính, chạy qua irm | iex, sẽ popup CMD menu
+$tmpBat = [System.IO.Path]::GetTempFileName() -replace '\.tmp$', '.bat'
+$batContent = @'
+@echo off
+powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://raw.githubusercontent.com/ranusTeam41/kinovrise/main/obsmcKinovRise.ps1 | iex"
+pause
+'@
+Set-Content -Encoding ASCII $tmpBat $batContent
+Start-Process cmd.exe -ArgumentList "/c `"$tmpBat`""
