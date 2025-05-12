f5bb8859803337493e9c062dbeb7bcc6a231cc1c
quinirix <pchidung57@outlook.com.vn>
2025-05-12 13:49:42 +0700

    Delete get.ps1

--- get.ps1
+++
@@ -1,5 +0,0 @@
-# KinovRise Bootstrapper
-$batUrl = "https://raw.githubusercontent.com/ranusTeam41/kinovrise/refs/heads/main/KinovRise.ps1"
-$tempBat = "$env:TEMP\KinovRise.bat"
-
-Invoke-WebRequest -Uri $batUrl -OutFile $tempBat
