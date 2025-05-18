dded8241d327e2f491a756bff50c5659e1b003d2
quinirix <pchidung57@outlook.com.vn>
2025-05-18 08:55:20 +0700

    Update KinovRiseMenu.bat

--- KinovRiseMenu.bat
+++ KinovRiseMenu.bat
@@ -4,14 +4,14 @@ chcp 65001
 cls
 echo -------------------------------------------
 echo   Activate license type 
-echo	[1] Activate 	Windows
-echo	[2] Activate 	Office
+echo [1] Activate- Windows
+echo [2] Activate- Office
 echo -------------------------------------------
 echo   Others
-echo	[3] Exit
+echo [3] Exit
 echo ============================================
 color 0A 
-set /p choice=		Select 1 of 3 functions ( 1 , 2 ,3 ) :
+set /p choice= Select 1 of 3 functions ( 1 , 2 ,3 ) :
 
 if "%choice%"=="1" (
     powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://raw.githubusercontent.com/ranusTeam41/kinovrise/main/Activate-Windows.ps1 | iex"
